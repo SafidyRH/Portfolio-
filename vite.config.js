28855efb882e31d22d5932d0657589780c88d6f7
Safidy RH <safidytiavina21@gmail.com>
2024-12-06 15:04:00 +0300

    migration to vite deployment file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Répertoire de sortie pour les fichiers de build
+  },
 })
